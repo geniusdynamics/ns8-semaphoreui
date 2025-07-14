eb087316cb1ef604b6f3f8c10978112df44591fd
Martin Bhuong <martin@genius.ke>
2025-07-14 11:21:23 +0000

    Upgrade semaphoreUI version to 2.15.0 from v2.10.22

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="semaphoreui"
-APP_VERSION="v2.10.22"
+APP_VERSION="v2.15.0"
 
 # Create a new empty container image
 container=$(buildah from scratch)
