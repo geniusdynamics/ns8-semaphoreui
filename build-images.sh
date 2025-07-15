934a9d421e1331034ae98abb9054c4f8195e7a33
Martin <nitram@genius.ke>
2025-07-15 10:51:06 +0200

    Add Cluster Account COnsumer in build Images

--- build-images.sh
+++ build-images.sh
@@ -43,7 +43,7 @@ buildah add "${container}" ui/dist /ui
 # rootfull=0 === rootless container
 # tcp-ports-demand=1 number of tcp Port to reserve , 1 is the minimum, can be udp or tcp
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@node:routeadm" \
+    --label="org.nethserver.authorizations=traefik@node:routeadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/semaphoreui/semaphore:${APP_VERSION}" \
